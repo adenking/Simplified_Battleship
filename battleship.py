1200ee49acde07edd682ab4230ef58d039082893
Aden King <adenking@live.com>
2017-08-23 17:45:06 +1000

    fixed up print board function -- invalid SYNTAX

--- battleship.py
+++ battleship.py
@@ -1,10 +1,11 @@
-from random import randint
-
 board = []
+for i in range(5):
+    board.append(["O"] * 5)
 
-for x in range(0, 5):
-  board.append(["O"] * 5)
 
-def print_board(board):
-  for row in board:
-    print " ".join(row)
+def print_board(board_in):
+    for row in board_in:
+        print(" ".join(row))
+
+
+print_board(board)
