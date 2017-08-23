3ff0e47a371bae7758cef00560d6163ff550d643
Aden King <adenking@live.com>
2017-08-23 22:22:22 +1000

    add turn counter after each guess

--- battleship.py
+++ battleship.py
@@ -21,17 +21,19 @@ def random_col(board_in):
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-guess_row = int(input("Guess Row: "))
-guess_col = int(input("Guess Col: "))
+for turn in range(4):
 
-if guess_row == ship_row and guess_col == ship_col:
-    print("Congratulations! You sank my battleship!")
-else:
-    if guess_row >= len(board) or guess_col >= len(board[0]):  # TODO add exception handling
-        print("Oops, that's not even in the ocean.")
-    elif board[guess_row][guess_col] == "X":
-        print("You guessed that one already.")
-    print("You missed my battleship!")
-    board[int(guess_col)][int(guess_row)] = "X"
-    print_board(board)
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
 
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratulations! You sank my battleship!")
+    else:
+        if guess_row >= len(board) or guess_col >= len(board[0]):  # TODO add exception handling
+            print("Oops, that's not even in the ocean.")
+        elif board[guess_row][guess_col] == "X":
+            print("You guessed that one already.")
+        print("You missed my battleship!")
+        board[int(guess_col)][int(guess_row)] = "X"
+        print('Turn: {0}'.format(turn+1))
+        print_board(board)
